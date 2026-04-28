b1e002b3d80f28c4b0969436be64b91e14cf5996
Flora De Checchi <flora.dechecchi@gmail.com>
2026-04-28 17:07:23 +0200

    ex 0en cours

--- ex0/construct.py
+++ ex0/construct.py
@@ -5,8 +5,8 @@ import site
 if __name__ == "__main__":
     if sys.prefix != sys.base_prefix:
         print("MATRIX STATUS: Welcome to the construct\n")
-        print(f"Current Python: {sys.base_prefix}")
-        print(f"Virtual Environnement: {sys.prefix}")
+        print(f"Current Python: {sys.executable}")
+        print(f"Virtual Environment: {sys.prefix}")
         print(f"Environment Path: {os.environ.get('VIRTUAL_ENV')}\n")
         print("SUCCESS: You're in an isolated environment!")
         print("Safe to install packages without affecting"
