b8093f35b4226fd18ac79d1b6ffb675235246ad4
flora <flora.dechecchi@gmailcom>
2026-04-29 23:04:15 +0200

    module 8 ex1 en cours debut avec .__name__ .__version__ et try except import error et impo module

--- ex0/construct.py
+++ ex0/construct.py
@@ -3,15 +3,35 @@ import os
 import site
 
 if __name__ == "__main__":
+    venv_name = os.path.basename(os.environ.get('VIRTUAL_ENV')) or "None detected"
+
     if sys.prefix != sys.base_prefix:
-        print("MATRIX STATUS: Welcome to the construct\n")
+        print("\nMATRIX STATUS: Welcome to the construct\n")
+
         print(f"Current Python: {sys.executable}")
-        print(f"Virtual Environment: {sys.prefix}")
-        print(f"Environment Path: {os.environ.get('VIRTUAL_ENV')}\n")
+        print(f"Virtual Environment: {venv_name}")
+        print(f"Environment Path: {sys.prefix}\n")
+
         print("SUCCESS: You're in an isolated environment!")
         print("Safe to install packages without affecting"
               " the global system.\n")
-        print(f"Package installation path: {site.getsitepackages()}")
+
+        print(f"Package installation path: {site.getsitepackages()[0]}")
 
     else:
-        print("MATRIX STATUS: You're still plugged in")
+        print("\nMATRIX STATUS: You're still plugged in\n")
+
+        print(f"Current Python: {sys.executable}")
+        print(f"Virtual Environment: {venv_name}\n")
+
+        print("""WARNING: You're in the global environment!)
+The machines can see everything you install.
+
+To enter the construct, run:
+python -m venv matrix_env
+source matrix_env/bin/activate # On Unix
+matrix_env\\Scripts\\activate # On Windows
+              
+Then run this program again.""")
+
+
