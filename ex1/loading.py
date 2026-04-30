4ce045b1994863e7fe2c0b0a7fd412ebda1c7380
flora <flora.dechecchi@gmailcom>
2026-04-30 14:26:51 +0200

    module 8 ex1

--- ex1/loading.py
+++ ex1/loading.py
@@ -1,23 +1,45 @@
 import sys
 import importlib
 
+png_name = "matrix_analysis.png"
 
 if __name__ == "__main__":
     try:
         pd = importlib.import_module("pandas")
         np = importlib.import_module("numpy")
         rq = importlib.import_module("requests")
-        mpb = importlib.import_module("matplotlib")
+        mbp = importlib.import_module("matplotlib")
     except ImportError:
-        print("Should show missing dependencies and installation instructions for pip and Poetry")
+        print("Erreur : Des modules sont manquants pour lancer la Matrice !")
+        print("-> Pour installer avec pip, tapez : pip install -r requirements.txt")
+        print("-> Pour installer avec Poetry, tapez : poetry install")
         sys.exit(1)
 
     print(f"""LOADING STATUS: Loading programs...
 [OK] {pd.__name__} ({pd.__version__}) - Data manipulation ready
 [OK] {np.__name__} ({np.__version__}) -  Numerical computation ready
 [OK] {rq.__name__} ({rq.__version__}) - Network access ready
-[OK] {mpb.__name__} ({mpb.__version__}) - Visualization ready
+[OK] {mbp.__name__} ({mbp.__version__}) - Visualization ready
 """)
+    
+    print("Analyzing Matrix data...")
+    print("Processing 1000 data points...")
+    
+    random_number = np.random.randint(0, 2, 1000)
+    df = pd.DataFrame(random_number, columns=["Matrix_Data"])
+
+    print("Generating visualization...\n")
+    plt = importlib.import_module("matplotlib.pyplot")
+    plt.plot(df.cumsum())
+    plt.savefig(png_name)
+    plt.show()
+    plt.close()
+
+    print("Analyzis complete!")
+    print(f"Results saved to: {png_name}")
+    
+    
+    
 
 # 1. Génère des fausses données "façon Matrix" en utilisant numpy
 #    calcul (interdiction d'écrire les données à la main, doit s'agir de 
